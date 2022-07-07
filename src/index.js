54baaa413496431edb8fb058b36f821ee7d1518e
timothyhead <48153209+timothyhead@users.noreply.github.com>
2022-07-07 17:25:50 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>OK OK OK</p>
+  </div>,
+  document.getElementById("root")
+);
